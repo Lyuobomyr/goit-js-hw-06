1cb9bb69e2ca82b66e2832eeb7161f22bd107ea2
Lyuobomyr Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-10-10 00:09:52 +0200

    done the eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === "" || password === "") {
+    return alert ("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+});
+
